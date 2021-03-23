985b350d47a0b84824631ba2f5a762040a1e7bbb
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2021-03-23 19:14:53 +0530

    WIP: map component
    Authors:
    - Soham
    - Pruthvi
    - Huzaif

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,18 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const MapWrapper = styled.div`
+  width: 100%;
+  height: 800px;
+`;
 
 const MapComponent = ({ locustData }) => {
-	return (
-		<>
-			<h1>MapComponent</h1>
-			{locustData.map((coordinate, idx) => (
-				<div key={idx}>
-					<h6>x:{coordinate.x}</h6>
-					<h6>y:{coordinate.y}</h6>
-					<hr />
-				</div>
-			))}
-		</>
-	);
+  console.log(locustData);
+  return (
+    <>
+      <h1>MapComponent</h1>
+
+      <MapWrapper>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ width: "100%", height: "800px" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {locustData.map((loc, idx) => {
+            return (
+              <Marker key={`abc${idx} + ${loc.x}`} position={[loc.y, loc.x]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MapContainer>
+      </MapWrapper>
+    </>
+  );
 };
 
 export default MapComponent;
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -4,34 +4,68 @@ import DefaultLayout from "layout";
 import MapComponent from "components/MapComponent";
 
 const MapPage = () => {
-	const [locustData, setLocustData] = useState([]);
-	const [coordinates, setCoordinates] = useState([]);
-	useEffect(() => {
-		fetch(
-			`https://services5.arcgis.com/sjP4Ugu5s0dZWLjd/arcgis/rest/services/Adults_Public/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json`
-		)
-			.then((response) => {
-				response.json().then((res) => {
-					console.log(res.features);
-					console.log(res.features[1].geometry);
-					res.features.map((x) => {
-						return setCoordinates(() => coordinates.push(x.geometry));
-					});
+  function inside(point, vs) {
+    // ray-casting algorithm based on
+    // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
 
-					setLocustData(coordinates);
-				});
-			})
+    var x = point[0],
+      y = point[1];
 
-			.catch((err) => console.log(err));
-		// useRef here to solve the eslint warning !important
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+      var xi = vs[i][0],
+        yi = vs[i][1];
+      var xj = vs[j][0],
+        yj = vs[j][1];
 
-	return (
-		<DefaultLayout>
-			<MapComponent locustData={locustData} />
-		</DefaultLayout>
-	);
+      var intersect =
+        yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+      if (intersect) inside = !inside;
+    }
+
+    return inside;
+  }
+  const indiaRegion = [
+    [64.034125, 33.016406],
+    [87.531118, 16.265597],
+    [76.355268, 37.45246],
+    [77.404398, 20.190844],
+  ];
+
+  const [locustData, setLocustData] = useState([]);
+  const [coordinates, setCoordinates] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://services5.arcgis.com/sjP4Ugu5s0dZWLjd/arcgis/rest/services/Adults_Public/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json`
+    )
+      .then((response) => {
+        response.json().then((res) => {
+          console.log(res.features);
+          console.log(res.features[1].geometry);
+          res.features.map((i) => {
+            return setCoordinates(() => {
+              console.log(inside([i.geometry.x, i.geometry.y], indiaRegion));
+              if (inside([i.geometry.x, i.geometry.y], indiaRegion)) {
+                return coordinates.push(i.geometry);
+              }
+            });
+          });
+
+          setLocustData(coordinates);
+        });
+      })
+
+      .catch((err) => console.log(err));
+
+    // useRef here to solve the eslint warning !important
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <DefaultLayout>
+      {locustData.length ? <MapComponent locustData={locustData} /> : null}
+    </DefaultLayout>
+  );
 };
 
 export default MapPage;
