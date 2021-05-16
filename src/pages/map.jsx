a078c8098731afc55e4e79340be7e17c0eaccf68
Soham Tamboli <sohamtamboli@gmail.com>
2021-05-17 00:07:34 +0530

    overall data

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -24,6 +24,24 @@ const MapPage = () => {
 
 		return inside;
 	}
+	function inside2(point, vs) {
+		var x = point[0],
+			y = point[1];
+
+		var inside = false;
+		for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+			var xi = vs[i][0],
+				yi = vs[i][1];
+			var xj = vs[j][0],
+				yj = vs[j][1];
+
+			var intersect =
+				yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+			if (intersect) inside = !inside;
+		}
+
+		return inside;
+	}
 	const indiaRegion = [
 		[64.034125, 33.016406],
 		[87.531118, 16.265597],
@@ -33,28 +51,41 @@ const MapPage = () => {
 
 	const [locustData, setLocustData] = useState([]);
 	const [coordinates, setCoordinates] = useState([]);
+	const coordinatesData = [];
 	useEffect(() => {
 		fetch(
 			`https://services5.arcgis.com/sjP4Ugu5s0dZWLjd/arcgis/rest/services/Adults_Public/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json`
 		)
 			.then((response) => {
 				response.json().then((res) => {
+					console.log("original resp", res);
 					console.log(res.features);
 					console.log(res.features[1].geometry);
 					res.features.map((i) => {
 						return setCoordinates(() => {
-							console.log(inside([i.geometry.x, i.geometry.y], indiaRegion));
+							// console.log(inside([i.geometry.x, i.geometry.y], indiaRegion));
 							if (inside([i.geometry.x, i.geometry.y], indiaRegion)) {
 								return coordinates.push(i.geometry);
 							}
 						});
 					});
-
+					res.features.map((i) => {
+						return setCoordinates(() => {
+							// console.log(inside([i.geometry.x, i.geometry.y], indiaRegion));
+							if (inside2([i.geometry.x, i.geometry.y], indiaRegion)) {
+								return coordinatesData.push(i);
+							}
+						});
+					});
 					setLocustData(coordinates);
+					console.log(JSON.stringify(coordinatesData, null, 2));
+					navigator.clipboard.writeText(JSON.stringify(coordinatesData));
 				});
 			})
 
 			.catch((err) => console.log(err));
+
+		console.log("India Region all data ", locustData.fields);
 
 		// useRef here to solve the eslint warning !important
 		// eslint-disable-next-line react-hooks/exhaustive-deps
